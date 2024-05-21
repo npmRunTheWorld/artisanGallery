a3b5d811b5a92082fc5186bdab4899c45ac4a4ac
acej0k3r <ace.rav3n@gmail.com>
2024-05-21 03:15:14 -0400

    added mock data

--- next.config.js
+++ next.config.js
@@ -2,9 +2,20 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io", // Corrected hostname
+        port: "",
+        pathname: "/f/**",
+      },
+    ],
+  },
+};
 
-export default config;
+export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+const domain = "https://utfs.io/f";
+
+const mockData = [
+  "0385c051-c673-47d7-9936-00a0c7f56c4c-3q905z.jpg",
+  "2184b181-45f0-4b8b-8cab-d6a65349f200-bhk2lc.jpg",
+  "cf67a279-8631-4447-8887-9cdd21712b26-rm6i6v.jpg",
+];
+
+const mockImage = mockData.map((image, index) => ({
+  id: index + 1,
+  url: `${domain}/${image}`,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>
-        <h1>Hello t3 gallery</h1>
+    <main className="">
+      <div className="d-flex flex-wrap">
+        {mockImage.map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <Image
+              src={image.url}
+              alt="image"
+              className="w-full"
+              width={500}
+              height={500}
+            />
+          </div>
+        ))}
       </div>
     </main>
   );
